1bc01f594656a02637626c42a6703b56b737275d
Who am I ? <azarov@game-insight.com>
2019-11-22 13:44:56 +0300

    v.2

---
+++ Address Book/Contacts.py
@@ -0,0 +1,11 @@
+import pprint from pprint
+
+class Contact (first_name, second_name, phone, favorite = False, **kwargs):
+    pass
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
---
+++ Address Book/PhoneBook.py
@@ -0,0 +1,23 @@
+class PhoneBook(name):
+    pb_file = phonebook.json
+
+    pass
+
+    def __init__(self):
+        pass
+
+    def print_book(self):
+        pass
+
+    def add_new(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def search_fav(self):
+        pass
+
+    def search_by(self):
+        pass
+
